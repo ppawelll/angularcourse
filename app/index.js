d50e4b9e4c2754d84305510d0965f99658be2b6e
Bartek <bslipek@gmail.com>
2016-03-22 17:51:26 +0000

    Adds home module

--- app/index.js
+++ app/index.js
@@ -5,6 +5,8 @@ import uirouter from 'angular-ui-router';
 
 import routing from './config';
 
+import home from './features/home';
+
 const ngModule = angular
-                    .module('app', [uirouter])
+                    .module('app', [uirouter, home])
                     .config(routing);
