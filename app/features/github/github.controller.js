1da0d8340f292d174f11472a571fcb6392737714
Bartek <bslipek@gmail.com>
2016-04-05 16:57:42 +0000

    Use $http to prepare request to github API

--- app/features/github/github.controller.js
+++ app/features/github/github.controller.js
@@ -1,3 +1,16 @@
 export default class GitHubController {
-
+  constructor($http) {
+    this.$http = $http;
+    
+    this.makeRequest();
+  }
+  
+  makeRequest() {
+    this.$http.get('https://api.github.com/users/bslipek')
+      .then(this.assignToScope.bind(this));
+  }
+  
+  assignToScope(response){
+    this.email = response.data.email;
+  }
 }
