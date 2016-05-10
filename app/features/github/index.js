2492a924668713331a8948105e981363b87e119c
Bartek <bslipek@gmail.com>
2016-05-10 13:58:52 +0000

    Adds description to github index

--- app/features/github/index.js
+++ app/features/github/index.js
@@ -1,9 +1,13 @@
 import angular from 'angular';
 import uirouter from 'angular-ui-router';
 
+// Import routing for this module
 import routing from './github.routes';
+
+// Import controllers for this module
 import GitHubController from './github.controller';
 
+// Import directives used by this module
 import githubUser from './directives/github_user';
 
 export default angular.module('app.github', [uirouter])
