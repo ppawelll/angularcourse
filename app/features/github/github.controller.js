98178d047910992c53ade159e233511cd2dbecf1
Bartek <bslipek@gmail.com>
2016-05-10 17:04:34 +0000

    Adds firebase to the application

--- app/features/github/github.controller.js
+++ app/features/github/github.controller.js
@@ -1,5 +1,6 @@
+/*global Firebase */
 export default class GitHubController {
-  constructor(GitHubService, $stateParams) {
+  constructor(GitHubService, $stateParams, $firebaseArray) {
     
     this.selectedUser = $stateParams.user
     
@@ -9,6 +10,9 @@ export default class GitHubController {
     GitHubService.getRepos(this.selectedUser)
       .then(this.handleGetReposSuccess.bind(this));
       
+    let ref = new Firebase("https://geoinformatyka.firebaseio.com/items");
+    this.items = $firebaseArray(ref);
+
   }
   
   handleGetUserSuccess(response) {
@@ -19,6 +23,10 @@ export default class GitHubController {
   handleGetReposSuccess(response) {
     this.Repos = response.data;
     console.log(this.Repos);
+  }
+  
+  addNote() {
+    this.data.$add({text: 'NOWY!'})
   }
  
 }
--- app/features/github/github.service.js
+++ app/features/github/github.service.js
@@ -4,10 +4,10 @@ export default class GitHubService {
   }
 
   getUser(user) {
-    return this.$http.get('https://api.github.com/users/' + user)
+    return this.$http.get('https://api.github.com/users/' + user + '?client_id=890dd92638638a6ca64c&client_secret=c400af0287a258d4f56f745e76b2d3b474d7e87a')
   }
   
   getRepos(user) {
-    return this.$http.get('https://api.github.com/users/'+ user +'/repos')
+    return this.$http.get('https://api.github.com/users/'+ user +'/repos?client_id=890dd92638638a6ca64c&client_secret=c400af0287a258d4f56f745e76b2d3b474d7e87a')
   }
 }
